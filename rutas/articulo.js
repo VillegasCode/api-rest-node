fcc9bf20f0488f97cc6eb185ecccfcc277b78ff8
Carlos Villegas <carlos.villegasch@gmail.com>
2023-07-12 18:15:27 -0500

    Creating routes to /crear /articulos/:ultimos? or /articulo/:id linking to their methods

--- rutas/articulo.js
+++ rutas/articulo.js
@@ -10,4 +10,12 @@ router.get("/curso", ArticuloControlador.curso);
 //RUTA ÚTIL
 router.post("/crear", ArticuloControlador.crear);
 
+//RUTA LISTAR ARTÍCULOS
+//Los parámetros opcionales inician con ":" y llevan un signo de cierre de interrogación al final
+router.get("/articulos/:ultimos?", ArticuloControlador.listar);
+
+//Los parámetros obligatorios no llevan ningún signo al final solo inician con ":"
+router.get("/articulo/:id", ArticuloControlador.uno)
+
+
 module.exports = router;
