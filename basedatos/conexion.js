9a6d1ac73ca9f3834fd7c1c36cb7fe8205247a6e
Carlos Villegas <carlos.villegasch@gmail.com>
2023-07-12 18:10:51 -0500

    Creating conexion to DB for port number 27017 through of puerto variable

--- basedatos/conexion.js
+++ basedatos/conexion.js
@@ -1,17 +1,18 @@
 const mongoose = require("mongoose");
+const puerto = 27017;
 
 const conexion = async() => {
 
     try {
 
-        await mongoose.connect("mongodb://127.0.0.1:27017/mi_blog");
+        await mongoose.connect("mongodb://127.0.0.1:" + puerto + "/mi_blog");
 
         //SOLO EN CASO DE AVISOS COLOCAR COMO PARÁMETROS A LA CONEXIÓN LO SIGUIENTE:
         // useNewUrlParser: true
         // useUnifiedTopology: true
         // useCreateIndex: true
 
-        console.log("Conectado correctamente a la base de datos mi_blog");
+        console.log("Conectado correctamente a la base de datos mi_blog en puerto: " + puerto);
 
     } catch(error) {
         console.log(error);
