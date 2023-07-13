a4e0fd28941ef17efc94e38f4a46909c43eec771
Carlos Villegas <carlos.villegasch@gmail.com>
2023-07-13 14:25:44 -0500

    Filter by recent records, by id, also listar, uno, borrar, editar, subir

--- rutas/articulo.js
+++ rutas/articulo.js
@@ -1,7 +1,20 @@
 const express = require("express");
+const multer = require("multer");
 const router = express.Router();
-
 const ArticuloControlador = require("../controladores/articulo");
+
+
+const almacenamiento = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, './imagenes/articulos/');
+    },
+
+    filename: function(req, file, cb) {
+        cb(null, "articulo" + Date.now() + file.originalname);
+    }
+})
+
+const subidas = multer({storage: almacenamiento});
 
 //Rutas de pruebas
 router.get("/ruta-de-prueba", ArticuloControlador.prueba);
@@ -17,5 +30,13 @@ router.get("/articulos/:ultimos?", ArticuloControlador.listar);
 //Los parámetros obligatorios no llevan ningún signo al final solo inician con ":"
 router.get("/articulo/:id", ArticuloControlador.uno)
 
+//Borrar/Eliminar un artículo de la BD
+router.delete("/articulo/:id", ArticuloControlador.borrar);
+
+//Editar/Cambiar/Actualizar un artículo de la BD
+router.put("/articulo/:id", ArticuloControlador.editar);
+
+//SUBIR IMAGEN con MIDLEWARE entre corchetes
+router.post("/subir-imagen/:id", [subidas.single("file0")], ArticuloControlador.subir);
 
 module.exports = router;
