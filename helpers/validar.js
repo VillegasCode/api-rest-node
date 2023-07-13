e1b8211e73ee344dacf2f3e1a3b4631da63a4c91
Carlos Villegas <carlos.villegasch@gmail.com>
2023-07-13 14:27:19 -0500

    Validate data like helper to this project

---
+++ helpers/validar.js
@@ -0,0 +1,16 @@
+const validator = require("validator");
+
+const validarArticulo = (parametros) => {
+    let validar_titulo = !validator.isEmpty(parametros.titulo) &&
+                        validator.isLength(parametros.titulo, { min: 2, max: undefined });
+
+    let validar_contenido = !validator.isEmpty(parametros.contenido);
+
+    if (!validar_titulo || !validar_contenido) {
+        throw new Error("No se ha validado la información!!");
+    } 
+};
+
+module.exports = {
+    validarArticulo
+}
